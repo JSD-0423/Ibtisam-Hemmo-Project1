849b14bfd1b8668523996c34642a2748f330a46c
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-05-12 21:03:39 +0300

    fix: Buttons' text content and icons when clicking

--- JS/index.js
+++ JS/index.js
@@ -3,19 +3,33 @@ import data from './courses.js';
 const coursesContainer = document.querySelector('.courses-container');
 const courses = document.querySelector('.courses');
 const darkModeBtn = document.querySelector('.dark-mode');
+const modeIcon = document.getElementById('mode-icon');
+const modeText = document.getElementById('mode-text');
 const favoritesBtn = document.querySelector('.favorites');
 const favPopUp = document.getElementById('popup-container');
+const favIcon = document.getElementById('fav-icon');
 
 darkModeBtn.addEventListener('click', () => {
   var root = document.querySelector(':root');
   root.classList.toggle('dark-mode');
+  if (modeIcon.getAttribute('name') === 'moon-outline') {
+    modeIcon.setAttribute('name', 'sunny-outline');
+    modeText.textContent = 'Light Mode';
+  } else {
+    modeIcon.setAttribute('name', 'moon-outline');
+    modeText.textContent = 'Dark Mode';
+  }
 })
 
 favoritesBtn.addEventListener('click', () => {
   if (favPopUp.style.display === 'block') {
     favPopUp.style.display = 'none';
+    favIcon.setAttribute('name', 'heart-outline');
+    favIcon.style.color = 'black';
   } else {
     favPopUp.style.display = 'block';
+    favIcon.setAttribute('name', 'heart');
+    favIcon.style.color = 'red';
   }
 })
 
