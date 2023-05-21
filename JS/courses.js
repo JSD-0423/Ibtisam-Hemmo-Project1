6a2494fc7fe781f2f317d7ee73b6045a3e09d278
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-05-21 22:49:21 +0300

    update data

--- JS/courses.js
+++ JS/courses.js
@@ -172,4 +172,124 @@ export default [
     rating: 70,
     type: "Back-end"
   },
+  {
+    image: "./assets/microservices.png",
+    description: "Backend Frameworks and Libraries",
+    title: "Microservices",
+    author: "John Doe",
+    rating: 90,
+    type: "Back-end"
+  },
+  {
+    image: "./assets/nosql.png",
+    description: "Backend Frameworks and Libraries",
+    title: "NoSQL",
+    author: "John Doe",
+    rating: 85,
+    type: "Back-end"
+  },
+  {
+    image: "./assets/oauth.png",
+    description: "Backend Frameworks and Libraries",
+    title: "Auth",
+    author: "John Doe",
+    rating: 40,
+    type: "Back-end"
+  },
+  {
+    image: "./assets/performance.png",
+    description: "Frontend Frameworks and Libraries",
+    title: "Performance",
+    author: "John Doe",
+    rating: 75,
+    type: "Front-end"
+  },
+  {
+    image: "./assets/pwa.png",
+    description: "Frontend Frameworks and Libraries",
+    title: "PWA",
+    author: "John Doe",
+    rating: 75,
+    type: "Front-end"
+  },
+  {
+    image: "./assets/responsive.gif",
+    description: "Frontend Frameworks and Libraries",
+    title: "Responsive",
+    author: "John Doe",
+    rating: 55,
+    type: "Front-end"
+  },
+  {
+    image: "./assets/restapi.jpeg",
+    description: "Backend Frameworks and Libraries",
+    title: "RestAPI",
+    author: "John Doe",
+    rating: 40,
+    type: "Front-end"
+  },
+  {
+    image: "./assets/seo.jpeg",
+    description: "Frontend Frameworks and Libraries",
+    title: "SEO",
+    author: "John Doe",
+    rating: 98,
+    type: "Front-end"
+  },
+  {
+    image: "./assets/sql.png",
+    description: "Backend Frameworks and Libraries",
+    title: "RestAPI",
+    author: "John Doe",
+    rating: 40,
+    type: "Front-end"
+  },
+  {
+    image: "./assets/testing.jpeg",
+    description: "Backend Frameworks and Libraries",
+    title: "Testing",
+    author: "John Doe",
+    rating: 54,
+    type: "Back-end"
+  },
+  {
+    image: "./assets/ux.jpeg",
+    description: "Frontend Frameworks and Libraries",
+    title: "UX",
+    author: "John Doe",
+    rating: 66,
+    type: "Front-end"
+  },
+  {
+    image: "./assets/ui.png",
+    description: "Frontend Frameworks and Libraries",
+    title: "UI",
+    author: "John Doe",
+    rating: 10,
+    type: "Front-end"
+  },
+  {
+    image: "./assets/wai.jpeg",
+    description: "Backend Frameworks and Libraries",
+    title: "WAI",
+    author: "John Doe",
+    rating: 100,
+    type: "Back-end"
+  },
+  {
+    image: "./assets/web-security.jpeg",
+    description: "Backend Frameworks and Libraries",
+    title: "Web Security",
+    author: "John Doe",
+    rating: 80,
+    type: "Back-end"
+  },
+  {
+    image: "./assets/websockets.png",
+    description: "Backend Frameworks and Libraries",
+    title: "Web Sockets",
+    author: "John Doe",
+    rating: 90,
+    type: "Back-end"
+  },
 ];
