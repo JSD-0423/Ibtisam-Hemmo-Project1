ab9f9667de59236e2c50e635d1bad5f2db6e852d
Ibtisam-Hemmo <ibtisamhemmo@gmail.com>
2023-06-16 23:21:23 +0300

    clean import export

--- src/components/Shared/index.js
+++ src/components/Shared/index.js
@@ -1,3 +1,4 @@
+export { default as Loading } from './Loading';
 export { default as Footer } from './Footer';
 export { default as FormInput } from './FormInput';
 export { default as Header } from './Header';
