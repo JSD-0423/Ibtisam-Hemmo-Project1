0bbc88105377d3281abdecc46261649a83d3d286
Ibtisam Hemmo <ibtisamhemmo@gmail.com>
2023-06-21 20:05:45 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,19 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// export default defineConfig(({ command }) => {
-//   const config = {
-//     plugins: [react()],
-//     base: '/',
-//   }
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
 
-//   if (command !== 'serve') {
-//     config.base = '/Ibtisam-Hemmo-Project1/'
-//   }
+  if (command !== 'serve') {
+    config.base = '/Ibtisam-Hemmo-Project1/'
+  }
 
-//   return config
-// })
-export default defineConfig({
-  base: "/",
-  plugins: [react()],
+  return config
 })
+// export default defineConfig({
+//   base: "/",
+//   plugins: [react()],
+// })
