5fbfd46c04e0ce1beb98718f1ef61c29dad115ad
Ibtisam Hemmo <ibtisamhemmo@gmail.com>
2023-06-22 10:06:33 +0300

    Update FavoriteItem.jsx

--- src/components/Shared/FavoriteItem.jsx
+++ src/components/Shared/FavoriteItem.jsx
@@ -4,7 +4,7 @@ const FavoriteItem = ({ image, rating, topic }) => {
   return (
     <div className="card border-0 custom-default-bg-color custom-shadow overflow-hidden rounded-1 card-w flex-shrink-0 fav-card">
       <img
-        src={`../../assets/images/${image}`}
+        src={`/Ibtisam-Hemmo-Project1/assets/images/${image}`}
         alt=""
         className="card-top-img fav-img bg-white"
       />
